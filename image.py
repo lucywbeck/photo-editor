f261ac7222724b05f906c6aad7259e0690ce6182
lucywbeck <76974109+lucywbeck@users.noreply.github.com>
2021-01-23 20:25:09 -0600

    Update image.py

--- image.py
+++ image.py
@@ -7,16 +7,25 @@ from PIL import Image as PIL_Image
 
 class Image(object):
     """
-    A class that allows flexible access to an image pixel list.
+    A class that stores image data.
     """
     @property
-    def source(self):
+    def original_source(self):
         """
-        The image source
+        The original image source
 
         Invariant: source is a valid image file
         """
-        return self._source
+        return self._original_source
+
+    @property
+    def current_source(self):
+        """
+        The current image source
+
+        Invariant: source is a valid image file
+        """
+        return self._current_source
 
     @property
     def width(self):
@@ -37,15 +46,13 @@ class Image(object):
         return self._height
 
     @property
-    def pix(self):
+    def current_pix(self):
         """
         The data at a pixel level
 
-        It can be accessed like a 2-dimensional list of 3-element tuples.
-
-        Invariant: pix is a PixelAccess Object
+        Invariant: current_pix is a 2-dimensional list of 3-element tuples
         """
-        return self._pix
+        return self._pix_history[-1]
 
     def __init__(self,file):
         """
@@ -55,7 +62,33 @@ class Image(object):
         Precondition: file is a valid image file
         """
         im = PIL_Image.open(file)
-        self._source = file
+        self._original_source = file
+        self._current_source = 'new.png'
         self._width = im.size[0]
         self._height = im.size[1]
-        self._pix = im.load()
+        self._original_pix = im.load()
+        self._pix_history = [self._original_pix]
+
+    def update_pix_history(self,pix):
+        """
+        Adds the most recent edit to the pixel history
+
+        If the number of elements in the pixel history exceeds 50 items,
+        the first element in the list will be removed from history.
+        """
+        self._pix_history.append(pix)
+        if len(self._pix_history) > 50:
+            self._pix_history.pop()
+
+    def undo(self):
+        """
+        Removes the most recent edit from the pixel history
+        """
+        if len(self._pix_history) > 1:
+            self._pix_history.pop()
+
+    def reset(self):
+        """
+        Removes all edits from the pixel history
+        """
+        self._pix_history = [self._original_pix]
